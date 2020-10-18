64a1abd93a705ac28f7614946daab2b7c3f3c9a9
Jeferson Cruz <jefersonhenri1@gmail.com>
2020-10-18 16:56:59 -0300

    Criando primeiros arquivos do projeto

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', function(){
+    return view('website.home');
+
+
+});
+
+
+
+Route::get('/category', function(){
+    return view('website.category');
+
+
+});
+Route::get('/contact', function(){
+    return view('website.contact');
+
+
+});
+Route::get('/post', function(){
+    return view('website.post');
+
+
+});
+Route::get('/about', function(){
+    return view('website.about');
+
+
+});
+
+// pagina do administrador rotas
+
+Route::get('/test', function(){
+    return view('admin.dashboard.index');
+
+});
